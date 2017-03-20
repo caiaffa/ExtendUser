0e9ecb701d5912d8d62b1fa30c0f4481fed3758a
Luís Felipe <lfa.luisfelipe@gmail.com>
2017-03-20 18:02:19 -0300

    Ajustes

--- accounts/models.py
+++ accounts/models.py
@@ -5,6 +5,9 @@ from django.db.models.signals import post_save
 from django.dispatch import receiver
 
 class Account(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    birth_date = models.DateField(null=True, blank=True)
+	user = models.OneToOneField(User, on_delete=models.CASCADE)
+	birth_date = models.DateField(null=True, blank=True)
 
+	class Meta:
+		verbose_name = 'Conta'
+		verbose_name_plural = 'Contas'
